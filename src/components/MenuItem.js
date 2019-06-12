dd21abe40c77be0fc23748286681d484e87320f6
marbaez1714 <marbaez1714@gmail.com>
2019-06-12 10:20:55 -0500

    adding menu items

--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -4,12 +4,9 @@ import "./MenuItem.scss";
 export default function MenuItem(props) {
   const { item } = props;
   return (
-    <div className="menuItemContainer">
-      <span className="menuItemName">
-        <i class="fas fa-plus menuItem " />
-        {item.name}
-      </span>
-      <div className="menuItemDisc">{item.disc}</div>
+    <div className="menuItem">
+      <div className="name">{item.name}</div>
+      <div>{item.disc}</div>
     </div>
   );
 }
--- src/components/MenuSection.js
+++ src/components/MenuSection.js
@@ -1,62 +1,43 @@
 import React, { useState } from "react";
+import MenuItem from "./MenuItem";
+import menu from "./menu.json";
 import "./MenuSection.scss";
 
 export default function MenuSection(props) {
-  const [menuKey, setMenuKey] = useState("lunchSpecials");
-
+  const [menuKey, setMenuKey] = useState(1);
+  console.log(menu);
   return (
     <div className={`parallaxSection menuContainer ${menuKey}`}>
       <div className="menuHeader">
-        <div
-          className={`menuSection appetizers ${menuKey === "appetizers" ? "active" : ""}`}
-          onClick={() => setMenuKey("appetizers")}
-        >
-          Appetizers
+        <div className={`section blue ${menuKey === 1 && "active"}`} onClick={() => setMenuKey(1)}>
+          Appetizers, Soups, & Salads
         </div>
         <div
-          className={`menuSection soupsSalads ${menuKey === "soupsSalads" ? "active" : ""}`}
-          onClick={() => setMenuKey("soupsSalads")}
+          className={`section orange ${menuKey === 2 && "active"}`}
+          onClick={() => setMenuKey(2)}
         >
-          Soups / Salads
+          House Specials & Mex-Tex
         </div>
-        <div
-          className={`menuSection houseSpecials ${menuKey === "houseSpecials" ? "active" : ""}`}
-          onClick={() => setMenuKey("houseSpecials")}
-        >
-          House Specials
-        </div>
-        <div
-          className={`menuSection mexTex ${menuKey === "mexTex" ? "active" : ""}`}
-          onClick={() => setMenuKey("mexTex")}
-        >
-          Mex-Tex
-        </div>
-        <div
-          className={`menuSection signatureTacos ${menuKey === "signatureTacos" ? "active" : ""}`}
-          onClick={() => setMenuKey("signatureTacos")}
-        >
+        <div className={`section green ${menuKey === 3 && "active"}`} onClick={() => setMenuKey(3)}>
           Signature Tacos
         </div>
-        <div
-          className={`menuSection lunchSpecials ${menuKey === "lunchSpecials" ? "active" : ""}`}
-          onClick={() => setMenuKey("lunchSpecials")}
-        >
-          Lunch Specails
+        <div className={`section red ${menuKey === 4 && "active"}`} onClick={() => setMenuKey(4)}>
+          Brunch & Lunch Specials
         </div>
-        <div
-          className={`menuSection brunch ${menuKey === "brunch" ? "active" : ""}`}
-          onClick={() => setMenuKey("brunch")}
-        >
-          Brunch
+        <div className={`section pink ${menuKey === 5 && "active"}`} onClick={() => setMenuKey(5)}>
+          Kids
         </div>
-        <div
-          className={`menuSection other ${menuKey === "other" ? "active" : ""}`}
-          onClick={() => setMenuKey("other")}
-        >
-          Kids / Desserts
+        <div className={`section teal ${menuKey === 6 && "active"}`} onClick={() => setMenuKey(6)}>
+          Drinks & Desserts
         </div>
       </div>
-      {menuKey}
+      <div className="menuBody">
+        <div className="menu">
+          {menu.map((item, idx) => (
+            <MenuItem item={item} key={item.name + idx} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
