7fc5d76664c7a3c637950b7aa05683d23a8ee179
Mario Baez <mbaez@researchnow.com>
2019-07-24 09:05:44 -0500

    Updates

---
+++ precache-manifest.45d755a3a64faabc1dc50be32fef0cff.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fd347c5be71246181da88e5e4e1ff75",
+    "url": "/pepes-mitos/index.html"
+  },
+  {
+    "revision": "f2980cd3dfaaa9e2e311",
+    "url": "/pepes-mitos/static/css/main.56053c6f.chunk.css"
+  },
+  {
+    "revision": "b2dfc16daacc6aba2286",
+    "url": "/pepes-mitos/static/js/2.6c0b87de.chunk.js"
+  },
+  {
+    "revision": "f2980cd3dfaaa9e2e311",
+    "url": "/pepes-mitos/static/js/main.9700e4af.chunk.js"
+  },
+  {
+    "revision": "d8e3d3417ae38ac04844",
+    "url": "/pepes-mitos/static/js/runtime~main.1ccdb5af.js"
+  },
+  {
+    "revision": "d7bcae5a319888d4698ef6bdc0c887ab",
+    "url": "/pepes-mitos/static/media/RecoletaBold.d7bcae5a.otf"
+  },
+  {
+    "revision": "ac8f3b34c9acc2fa8039d496d28781e5",
+    "url": "/pepes-mitos/static/media/RecoletaRegular.ac8f3b34.otf"
+  },
+  {
+    "revision": "b043e60514e37e0e1a0735e864f2676f",
+    "url": "/pepes-mitos/static/media/background.b043e605.jpg"
+  },
+  {
+    "revision": "a5a21ba982dda081b1a870d689e28c3d",
+    "url": "/pepes-mitos/static/media/background1-3.a5a21ba9.jpg"
+  },
+  {
+    "revision": "490fb50471c4d623a3fa13f8f1d40d80",
+    "url": "/pepes-mitos/static/media/background2-3.490fb504.jpg"
+  },
+  {
+    "revision": "081f539ef2f98ff83daff19234996326",
+    "url": "/pepes-mitos/static/media/background3-3.081f539e.jpg"
+  },
+  {
+    "revision": "df6fbbcd58ed962fc0ef749a35fc5857",
+    "url": "/pepes-mitos/static/media/coaster.df6fbbcd.png"
+  },
+  {
+    "revision": "0277c785a3840d5552a683eb740b46cb",
+    "url": "/pepes-mitos/static/media/concrete-wall.0277c785.png"
+  },
+  {
+    "revision": "2547ffd1c56d9ac46b1e5ef352028001",
+    "url": "/pepes-mitos/static/media/frontpage.2547ffd1.jpg"
+  },
+  {
+    "revision": "49e3c417b12969b502320502fca009b6",
+    "url": "/pepes-mitos/static/media/guyhead.49e3c417.png"
+  },
+  {
+    "revision": "8fcc731b7a33beb8883f249de0b354c0",
+    "url": "/pepes-mitos/static/media/lined-paper-2.8fcc731b.png"
+  },
+  {
+    "revision": "20be14190d82235d735c41709af09fc6",
+    "url": "/pepes-mitos/static/media/pm.20be1419.png"
+  }
+]);
