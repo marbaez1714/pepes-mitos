2a2f80841201cfc8a2dfb173069cf01b5ee9d8d1
marbaez1714 <mario.baez@dialexa.com>
2019-10-17 21:14:57 -0500

    contact info

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -3,37 +3,7 @@ import "./Contact.scss";
 import GoogleMapReact from "google-map-react";
 
 const Pipe = <span className="section-pipe">||</span>;
-const PhoneNumber = (
-  <div>
-    <a href="tel:214-741-1901">214-741-1901</a>
-  </div>
-);
-const Hours = (
-  <React.Fragment>
-    <div className="section-title">Hours</div>
-    <div className="section-content">
-      <div>Mon. & Tue. {Pipe} 11am - 3pm</div>
-      <div>Wed. & Thu. {Pipe} 11am - 10pm</div>
-      <div>Fri. & Sat. {Pipe} 11am - 11pm</div>
-      <div>Sun. {Pipe} 11am - 5pm</div>
-    </div>
-  </React.Fragment>
-);
-const Location = (
-  <React.Fragment>
-    <div className="section-title">Location</div>
-    <div className="location">
-      Downtown Dallas, deep in the heart of the historic Deep Ellum District
-    </div>
-    <a
-      href="https://goo.gl/maps/pEJBpcGrtMF4rEic8"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      2911 Elm Street, Dallas, Texas 75226
-    </a>
-  </React.Fragment>
-);
+
 
 export default function Contact(props) {
   const API_KEY = process.env.REACT_APP_MAP_API_KEY;
@@ -54,23 +24,56 @@ export default function Contact(props) {
   };
   return (
     <div className="contact-page">
-      <div className="filter" />
-      <div className="contact-info">
-        {Hours}
-        <div className="section-title">Catering & Events</div>
-        {PhoneNumber}
-        <div className="section-title">Questions & Reservations</div>
-        {PhoneNumber}
-        {Location}
-        {/* <div className="map-wrapper">
-          <GoogleMapReact
-            bootstrapURLKeys={{ key: API_KEY }}
-            defaultCenter={center}
-            defaultZoom={17}
-            yesIWantToUseGoogleMapApiInternals
-            onGoogleApiLoaded={({ map, maps }) => handleApiLoaded(map, maps)}
-          />
-        </div> */}
+      <div className="contact-section">
+        <div className="section-title yellow">
+          <p className="title">Hours</p>
+        </div>
+        <div className="section-content">
+          <div>Mon. & Tue. {Pipe} 11am - 3pm</div>
+          <div>Wed. & Thu. {Pipe} 11am - 10pm</div>
+          <div>Fri. & Sat. {Pipe} 11am - 11pm</div>
+          <div>Sun. {Pipe} 11am - 5pm</div>
+        </div>
+      </div>
+      <div className="contact-section">
+        <div className="section-title phone blue">
+          <p className="title">Phone Number</p>
+        </div>
+        <div className="section-content">
+          For reservations, catering, events, and all other questions, call :
+          <a href="tel:214-741-1901">214-741-1901</a>
+        </div>
+      </div>
+      <div className="contact-section">
+        <div className="section-title orange">
+          <p className="title">Location</p>
+        </div>
+        <div className="section-content">
+          <div>Deep in the heart of the historic Deep Ellum District</div>
+          <a
+            href="https://goo.gl/maps/pEJBpcGrtMF4rEic8"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            2911 Elm Street
+          </a>
+        </div>
+      </div>
+      <div className="contact-section">
+        <div className="section-title green">
+          <p className="title">Map</p>
+        </div>
+        <div className="section-content">
+          <div className="map-wrapper">
+            <GoogleMapReact
+              bootstrapURLKeys={{ key: API_KEY }}
+              defaultCenter={center}
+              defaultZoom={17}
+              yesIWantToUseGoogleMapApiInternals
+              onGoogleApiLoaded={({ map, maps }) => handleApiLoaded(map, maps)}
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
